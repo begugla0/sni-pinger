b670682841d06f270dc27976f650df3aeeffda7a
begugla0 <maksladygen@yandex.ru>
2026-04-12 00:53:19 +0500

    1.3.0-BETA_FIX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 5
-        versionName = "1.3.0"
+        versionName = "1.3.0-BETA"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
